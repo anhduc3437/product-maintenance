8f99de285fcb826685130482ad0734f0e00c0f97
Hieunguyen <hieunguyen08102001@gmail.com>
2022-05-07 09:30:29 +0700

    add model class

--- src/main/java/com/dduwcs/model/Product.java
+++ src/main/java/com/dduwcs/model/Product.java
@@ -1,5 +1,23 @@
 package com.dduwcs.model;
 
-public class Product {
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "products")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class Product {
+    @Id
+    private String code;
+    private String description;
+    private Double price;
 }
